173129ba9a53fab53d8f1b4e87c8e565f5c52f0c
mohidul31 <mislam6789@gmail.com>
2018-05-11 21:57:31 +0600

    Home View, Search, Filter Added

--- Blogging-Times.Web/Controllers/HomeController.cs
+++ Blogging-Times.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blogging_Times.Posts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,24 @@ namespace Blogging_Times.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(Guid? tag,Guid ? category,int? month,int? year,string search)
         {
             ViewBag.Title = "Home Page";
+            ViewBag.Search = search;
 
-            return View();
+            var result = new HomeViewModel(tag,category,month,year,null, search);
+
+            return View(result);
+        }
+
+        public ActionResult PostDetails(Guid? post)
+        {
+            ViewBag.Title = "Post Page";
+            ViewBag.Search = "";
+
+            var result = new HomeViewModel(null,null,null,null,post);
+
+            return View(result);
         }
     }
 }
